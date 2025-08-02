9dab67c07087fe6487d3f81d4e60520dd7033bfa
babanini95 <arobbanii5@gmail.com>
2025-08-03 04:53:30 +0800

    Init

---
+++ cmd/root.go
@@ -0,0 +1,22 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "diaper-duty",
+	Short: "A CLI tool to track baby diaper changes.",
+	Long: `Diaper Duty is a simple CLI tool to help you remember
+when your baby's diaper was last changed.`,
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/babanini95/diaper-duty/cmd"
+
+func main() {
+	cmd.Execute()
+}
