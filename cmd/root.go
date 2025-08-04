3c94a45fd996a3ef889743a6945746e375ed9dc8
babanini95 <arobbanii5@gmail.com>
2025-08-04 09:30:42 +0800

    feat: add persistent pre run in root

--- cmd/root.go
+++ cmd/root.go
@@ -1,7 +1,9 @@
 package cmd
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -12,14 +14,27 @@ var rootCmd = &cobra.Command{
 	Short: "A CLI tool to track baby diaper changes.",
 	Long: `Diaper Duty is a simple CLI tool to help you remember
 when your baby's diaper was last changed.`,
+	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+		if cmd.Name() == "help" || cmd.Name() == "completion" || cmd.Name() == "init" {
+			return nil
+		}
+		count, err := appState.db.CountProfiles(context.Background())
+		if err != nil {
+			return fmt.Errorf("database check failed: %w", err)
+		}
+
+		if count == 0 {
+			return fmt.Errorf("project not initialized. Please run 'diaper-duty init' first")
+		}
+		return nil
+	},
 }
 
 func Execute() {
 	appState = CreateNewState()
 	err := CreateQueries()
 	if err != nil {
-		fmt.Printf("Cannot set database: %v", err)
-		os.Exit(1)
+		log.Fatalf("Cannot set database: %v", err)
 	}
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
